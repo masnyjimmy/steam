984bf43e985748fa4d4770decb78c23bee0eab0e
masnyjimmy <101148919+masnyjimmy@users.noreply.github.com>
2025-04-28 09:41:17 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+
+
+<body>
+    <ul>
+        <li><a href="games/index.php">Games</a></li>
+        <li><a href="users/index.php">Users</a></li>
+        <li><a href="developers/index.php">Developers</a></li>
+        <li><a href="publishers/index.php">Publishers</a></li>
+        <li><a href="tags/index.php">Tags</a></li>
+        <li><a href="platform/index.php">Platforms</a></li>
+    </ul>
+</body>
+
+</html>
